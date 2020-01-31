b899a7cc0536299c391cff7894853e4a826fdb37
Brian <Brian@Brians-MacBook-Air.local>
2020-01-31 12:00:20 -0600

    all eror functions done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const emailError = document.querySelector('.email-error');
 const password = document.getElementById('password');
 const passwordError = document.querySelector('.password-error');
 const passwordTwo = document.getElementById('password2')
-const passwordTwoError = document.querySelector('password2.-error');
+const passwordTwoError = document.querySelector('password2-error');
 const btn = document.getElementById('btn');
 
 
@@ -16,12 +16,73 @@ function subMit(e){
   console.log("you clicked me!")
 
 
-  nameForm()
+  emailForm();
+  nameForm();
+  passChecker();
   e.preventDefault();
 }
-
+//////////////////////////////////////////////////////////////////////
 function nameForm(){
-  if(name.value.length < 7){
-    nameError.innerHTML = "Please enter a password that is at lease 7 charcters"
+  let containsNum = name.value.match(/[0-9]/g) 
+  let whiteSpace = name.value.match(/\s/g);
+    
+    if(containsNum){
+      nameError.innerHTML = "Why Does your name Have Numbers!!!???"
+    }
+    if(whiteSpace){
+      nameError.innerHTML = "There should be spaces"
+    }
+    
+    if(name.value.length < 4){
+      nameError.innerHTML = "Please enter a password that is at lease 3 charcters"
+    }
+
+      setTimeout(() =>{
+        nameError.innerHTML = ""
+      }, 3000)
+}
+/////////////////////////////////////
+function emailForm (){
+
+    // Courtesy of https://emailregex.com/ ///
+    let success = email.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    if(!success){
+        emailError.innerHTML = "Please Enter a Valid Email"
+      }
+      setTimeout(() =>{
+        emailError.innerHTML = ""
+      }, 3000)
+}
+
+////////////////////////////////////
+function passChecker(){
+  let capital = password.value.match(/[A-Z]/g);
+  let specialCharcter = password.value.match(/[$-/:-?{-~!"^_`\[\]]/g);
+  let numbers = password.value.match(/[0-9]/g)
+  
+  if(!capital){
+    passwordError.innerHTML = 'Must contain at least one Capital letter';
   }
+  if(password.value < 8){
+    passwordError.innerHTML = 'Must be at least 8 charachter long';
+  }
+  if(!specialCharcter){
+    passwordError.innerHTML = 'Must have at least one special Character';
+  }
+  if(!numbers){
+    passwordError.innerHTML = 'Must have at least one number';
+  }
+  
+  setTimeout(() =>{
+    passwordError.innerHTML = ""
+  }, 3000)
+
+  if(password.value !== passwordTwo.value){
+    passwordTwoError.innerHTML = 'Passwords dont match'
+
+    setTimeout(() =>{
+      passwordTwoError.innerHTML = ""
+    }, 3000)
+  }
+  
 }
